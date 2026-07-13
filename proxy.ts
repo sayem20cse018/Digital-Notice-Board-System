e550e367a6ff104240b36cd68e8a921ff51c1150
sayem20cse018 <musacse10@gmail.com>
2026-07-13 23:00:37 +0600

    fix: rename proxy function name from middleware to proxy in proxy.ts

--- proxy.ts
+++ proxy.ts
@@ -2,7 +2,7 @@ import { withAuth } from "next-auth/middleware";
 import { NextResponse } from "next/server";
 
 export default withAuth(
-	function middleware(req) {
+	function proxy(req) {
 		const response = NextResponse.next();
 		response.headers.set("x-url", req.url);
 		response.headers.set("x-pathname", req.nextUrl.pathname);
